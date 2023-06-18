6cb6198a6776c9fcd4f0e109f696c28e95eec053
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-06-18 15:20:15 +0300

    delete comment

--- contacts.js
+++ contacts.js
@@ -6,7 +6,6 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db/contacts.json");
 console.log(contactsPath);
 
-// TODO: задокументувати кожну функцію
 async function listContacts() {
 	const contactsData = await fs.readFile(contactsPath);
 	return JSON.parse(contactsData);
